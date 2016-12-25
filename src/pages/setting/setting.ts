ebbc3209ecb516f744963252550863ea03f0c8ab
Wasut <wasut_pao@hotmail.com>
2016-12-25 15:21:48 +0700

    inject setting service show default to select and add method setDefault

--- src/pages/setting/setting.ts
+++ src/pages/setting/setting.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
-
+import { SettingService } from '../../app/services/setting.service';
 import { NavController } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
 @Component({
   selector: 'page-setting',
   templateUrl: 'setting.html'
 })
 export class SettingPage {
+  category: any
+  limit: any
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private settingService: SettingService) {
+  	this.category = this.settingService.getCategory()
+    this.limit = this.settingService.getLimit()
   }
 
+  setDefault(){
+  	this.settingService.setDefault(this.category, this.limit)
+
+  	this.navCtrl.push(HomePage)
+  }
 }
